de4c66956567182af996be5c494d30318380f862
Lightrain131 <lightraincybless@gmail.com>
2023-11-07 03:23:12 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def quadraticFormula1(a,b,c):
+    x = (-b+(b**2-4*a*c)**0.5)/(2*a)
+    return(x)
+def quadraticFormula2(a,b,c):
+    y = (-b-(b**2-4*a*c)**0.5)/(2*a)
+    return(y)
+print(quadraticFormula1(1,8,9))
+print(quadraticFormula2(1,8,9))
